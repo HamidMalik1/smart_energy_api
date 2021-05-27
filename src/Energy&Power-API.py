0021e3b1466e549b453061e855705b6ec9d4a499
Hamid Malik <79519183+HamidMalik1@users.noreply.github.com>
2021-05-27 22:56:37 +0300

    Final commit

---
+++ src/Energy&Power-API.py
@@ -0,0 +1,135 @@
+'''
+Created on 10.05.2021
+Provides the SolarEdge API data of Uni of Oulu
+'''
+
+
+import solaredge
+import schedule
+import requests
+import MySQLdb
+from datetime import datetime, timedelta
+import csv
+import pandas as pd
+import time
+
+
+
+
+
+today = datetime.today().strftime('%Y-%m-%d')
+print(today)
+yesterday = datetime.now() - timedelta(1)
+yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
+print(yesterday)
+
+
+
+api_key = 'I8AZZW5B2XGFNM3WSJ8IDA0441Z9TQ9V'
+site_id = '1703225'
+
+
+def job1():
+
+
+    api_url_site_overview = 'https://monitoringapi.solaredge.com/site/' + site_id + '/overview.json?api_key=' + api_key
+    site_overview_json_data = requests.get(api_url_site_overview).json()
+
+    class overview():
+        @staticmethod
+        def site_overview():
+            lastupdatetime = site_overview_json_data['overview']['lastUpdateTime']
+            lifetimedata = site_overview_json_data['overview']['lifeTimeData']['energy'] / 1000
+            lastyearenergy = site_overview_json_data['overview']['lastYearData']['energy'] / 1000
+            lastmonthenergy = site_overview_json_data['overview']['lastMonthData']['energy'] / 1000
+            lastdayenergy = site_overview_json_data['overview']['lastDayData']['energy'] / 1000
+            currentpower = site_overview_json_data['overview']['currentPower']['power']
+            return {'lastupdatetime': lastupdatetime, 'lifetimedata': lifetimedata,
+                    'lastyearenergy': lastyearenergy, 'lastmonthenergy': lastmonthenergy, 'lastdayenergy': lastdayenergy,
+                    'currentpower': currentpower}
+
+
+    result4 = overview.site_overview()
+
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="5gtnoulu", db="smartmetering")
+    sql_statement = "INSERT INTO solaredge_overview_api(lastupdatetime ,lifetimedata, lastyearenergy,lastmonthenergy,lastdayenergy, currentpower )  VALUES (%s,%s,%s,%s,%s,%s)"
+    cur = conn.cursor()
+    cur.executemany(sql_statement, [(result4['lastupdatetime'], result4['lifetimedata'], result4['lastyearenergy'],
+                                    result4['lastmonthenergy'], result4['lastdayenergy'], result4['currentpower'])])
+
+    conn.escape_string(sql_statement)
+    conn.commit()
+
+    return
+
+
+
+#Energy and Power Production details
+
+def job2():
+
+    api_data = solaredge.Solaredge("I8AZZW5B2XGFNM3WSJ8IDA0441Z9TQ9V")
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="5gtnoulu", db="smartmetering")
+
+    # Edit this date range as you see fit
+    # If querying at the maximum resolution of 15 minute intervals, the API is limited to queries of a month at a time
+    # This script queries one day at a time, with a one-second pause per day that is polite but probably not necessary
+
+    day_list = pd.date_range(start=yesterday, end=today)
+    day_list = day_list.strftime('%Y-%m-%d')
+
+    energy_df_list = []
+
+    for day in day_list:
+        temp = api_data.get_energy_details(site_id, day + ' 00:00:00', day + ' 23:59:59', time_unit='QUARTER_OF_AN_HOUR')
+        temp_df = pd.DataFrame(temp['energyDetails']['meters'][0]['values'])
+        energy_df_list.append(temp_df)
+        time.sleep(1)
+
+    power_df_list = []
+
+    for day in day_list:
+        temp = api_data.get_power_details(site_id, day + ' 00:00:00', day + ' 23:59:59')
+        temp_df = pd.DataFrame(temp['powerDetails']['meters'][0]['values'])
+        power_df_list.append(temp_df)
+        time.sleep(1)
+
+    energy_df = pd.concat(energy_df_list)
+    energy_df.columns = ['date', 'energy']
+    power_df = pd.concat(power_df_list)
+    power_df.columns = ['date', 'power']
+
+    merged = pd.merge(energy_df, power_df)
+    print(energy_df, power_df)
+
+    merged.to_csv("C:/Users/hamalik/Desktop/pythonProject/Solaredge-FMI/energy_power_production.csv'", index=False)
+
+    with open('C:/Users/hamalik/Desktop/pythonProject/Solaredge-FMI/energy_power_production.csv') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=',')
+
+
+        for row in reader:
+
+            sql_statement = "INSERT INTO energy_power_production(date ,energy ,power)  VALUES (%s,%s,%s)"
+            cur = conn.cursor()
+            cur.executemany(sql_statement, [(row['date'], row['energy'], row['power'])])
+
+            conn.escape_string(sql_statement)
+            conn.commit()
+
+        sql_Delete_query = """DELETE n1 FROM energy_power_production n1, energy_power_production n2 WHERE n1.ID < n2.ID AND n1.date = n2.date """
+        cur.execute(sql_Delete_query)
+        conn.commit()
+        print('number of rows deleted', cur.rowcount)
+
+    return
+
+
+schedule.every(5).minutes.do(job1)
+schedule.every(15).minutes.do(job2)
+
+while True:
+    schedule.run_pending()
+
+
--- src/SolarEdgeAPI.py
+++ src/SolarEdgeAPI.py
@@ -3,6 +3,20 @@ Created on 10.05.2021
 Provides the SolarEdge API data of Uni of Oulu
 '''
 import requests
+import schedule
+import MySQLdb
+from datetime import datetime, timedelta
+
+
+
+
+
+today = datetime.today().strftime('%Y-%m-%d')
+print(today)
+yesterday = datetime.now() - timedelta(1)
+yesterday = datetime.strftime(yesterday, '%Y-%m-%d')
+print(yesterday)
+
 
 #api_data = solaredge.Solaredge("I8AZZW5B2XGFNM3WSJ8IDA0441Z9TQ9V")
 
@@ -13,14 +27,11 @@ site_id = '1703225'
 api_url_meters = 'https://monitoringapi.solaredge.com/site/' + site_id + '/meters?&startTime=2021-05-24%2000:00:00&endTime=2021-05-24%2023:00:00&api_key=' + api_key
 meters_json_data = requests.get(api_url_meters).json()
 
-api_url_site_energy = 'https://monitoringapi.solaredge.com/site/' + site_id + '/energy?timeUnit=QUARTER_OF_AN_HOUR&endDate=2021-05-24&startDate=2021-05-24&api_key=' + api_key
+api_url_site_energy = 'https://monitoringapi.solaredge.com/site/' + site_id + '/energy?timeUnit=QUARTER_OF_AN_HOUR&endDate=' + today + '&startDate=' + yesterday + '&api_key=' + api_key
 site_energy_json_data = requests.get(api_url_site_energy).json()
 
 api_url_site_power = 'https://monitoringapi.solaredge.com/site/' + site_id + '/power?startTime=2021-05-24%2000:00:00&endTime=2021-05-25%2023:00:59&api_key=' + api_key
 site_power_json_data = requests.get(api_url_site_power).json()
-
-api_url_site_overview = 'https://monitoringapi.solaredge.com/site/' + site_id + '/overview.json?api_key=' + api_key
-site_overview_json_data = requests.get(api_url_site_overview).json()
 
 api_url_site_energy_details = 'https://monitoringapi.solaredge.com/site/' + site_id + '/energyDetails?meters=PRODUCTION,CONSUMPTION&timeUnit=DAY&startTime=2021-05-15%2011:00:00&endTime=2021-05-16%2013:00:0&api_key=' + api_key
 site_energy_detail_json_data = requests.get(api_url_site_energy_details).json()
@@ -57,121 +68,147 @@ result = solaredgemeters.meterdata()
 print(result['meter_api'])
 
 
-class siteenergy():
-    @staticmethod
-    def energydata():
-        energy_api = site_energy_json_data['energy']['values']
-
-        return {'energy_api': energy_api}
 
 
-result2 = siteenergy.energydata()
-#print(result2['energy_api'])
+def job1():
 
 
-class sitepower():
-    @staticmethod
-    def powerdata():
-        power_api = site_power_json_data['power']['values']
+    api_url_site_overview = 'https://monitoringapi.solaredge.com/site/' + site_id + '/overview.json?api_key=' + api_key
+    site_overview_json_data = requests.get(api_url_site_overview).json()
 
-        return {'power_api': power_api}
-
-
-result3 = sitepower.powerdata()
-#print(result3['power_api'])
-
-
-class overview():
-    @staticmethod
-    def site_overview():
-        lastupdatetime = site_overview_json_data['overview']['lastUpdateTime']
-        lifetimedata = site_overview_json_data['overview']['lifeTimeData']['energy'] / 1000
-        lastyearenergy = site_overview_json_data['overview']['lastYearData']['energy'] / 1000
-        lastmonthenergy = site_overview_json_data['overview']['lastMonthData']['energy'] / 1000
-        lastdayenergy = site_overview_json_data['overview']['lastDayData']['energy'] / 1000
-        currentpower = site_overview_json_data['overview']['currentPower']['power']
-        return {'lastupdatetime': lastupdatetime, 'lifetimedata': lifetimedata,
-                'lastyearenergy': lastyearenergy, 'lastmonthenergy': lastmonthenergy, 'lastdayenergy': lastdayenergy,
-                'currentpower': currentpower}
+    class overview():
+        @staticmethod
+        def site_overview():
+            lastupdatetime = site_overview_json_data['overview']['lastUpdateTime']
+            lifetimedata = site_overview_json_data['overview']['lifeTimeData']['energy'] / 1000
+            lastyearenergy = site_overview_json_data['overview']['lastYearData']['energy'] / 1000
+            lastmonthenergy = site_overview_json_data['overview']['lastMonthData']['energy'] / 1000
+            lastdayenergy = site_overview_json_data['overview']['lastDayData']['energy'] / 1000
+            currentpower = site_overview_json_data['overview']['currentPower']['power']
+            return {'lastupdatetime': lastupdatetime, 'lifetimedata': lifetimedata,
+                    'lastyearenergy': lastyearenergy, 'lastmonthenergy': lastmonthenergy, 'lastdayenergy': lastdayenergy,
+                    'currentpower': currentpower}
 
 
-result4 = overview.site_overview()
-print(result4['lastyearenergy'])
+    result4 = overview.site_overview()
 
 
+    conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="5gtnoulu", db="smartmetering")
+    sql_statement = "INSERT INTO Solar_Production_Flow(lastupdatetime ,lifetimedata, lastyearenergy,lastmonthenergy,lastdayenergy, currentpower )  VALUES (%s,%s,%s,%s,%s,%s)"
+    cur = conn.cursor()
+    cur.executemany(sql_statement, [(result4['lastupdatetime'], result4['lifetimedata'], result4['lastyearenergy'],
+                                    result4['lastmonthenergy'], result4['lastdayenergy'], result4['currentpower'])])
 
-class siteenergydetails():
-    @staticmethod
-    def energydetailsdata():
-        energy_details_api = site_energy_detail_json_data['energyDetails']
+    conn.escape_string(sql_statement)
+    conn.commit()
 
-        return {'energy_details_api': energy_details_api}
-
-
-result5 = siteenergydetails.energydetailsdata()
-#print(result5['energy_details_api'])
-
+    return
 
 
-
-class sitepowerflow():
-    @staticmethod
-    def powerflowdata():
-        powerflow_api = site_powerflow_json_data['siteCurrentPowerFlow']
-
-        return {'powerflow_api': powerflow_api}
+def job2():
 
 
-result6 = sitepowerflow.powerflowdata()
-print(result6['powerflow_api'])
+    class siteenergy():
+        @staticmethod
+        def energydata():
+            energy_api = site_energy_json_data['energy']['values']
+
+            return {'energy_api': energy_api}
+
+    result2 = siteenergy.energydata()
+
+    # print(result2['energy_api'])
+
+    class sitepower():
+        @staticmethod
+        def powerdata():
+            power_api = site_power_json_data['power']['values']
+
+            return {'power_api': power_api}
+
+    result3 = sitepower.powerdata()
+
+    # print(result3['power_api'])
 
 
 
-class sitestorage():
-    @staticmethod
-    def storagedata():
-        storage_api = site_storage_json_data['storageData']
+    class siteenergydetails():
+        @staticmethod
+        def energydetailsdata():
+            energy_details_api = site_energy_detail_json_data['energyDetails']
 
-        return {'storage_api': storage_api}
-
-
-result7 = sitestorage.storagedata()
-print(result7['storage_api'])
+            return {'energy_details_api': energy_details_api}
 
 
-
-class siteenvbenefits():
-     @staticmethod
-     def envdata():
-         envbenefits_api = site_env_benefits_json_data['envBenefits']
-
-         return {'envbenefits_api': envbenefits_api}
-
-
-result8 = siteenvbenefits.envdata()
-print(result8['envbenefits_api'])
+    result5 = siteenergydetails.energydetailsdata()
+    #print(result5['energy_details_api'])
 
 
 
-class siteinverter():
-    @staticmethod
-    def inverterdata():
-        inverter_api = site_inverter_json_data['data']
 
-        return {'inverter_api': inverter_api}
+    class sitepowerflow():
+        @staticmethod
+        def powerflowdata():
+            powerflow_api = site_powerflow_json_data['siteCurrentPowerFlow']
 
-result9 = siteinverter.inverterdata()
-#print(result9['inverter_api'])
+            return {'powerflow_api': powerflow_api}
 
 
-class sitesensors():
-    @staticmethod
-    def sensordata():
-        sensor_api = site_sensor_json_data
+    result6 = sitepowerflow.powerflowdata()
+    print(result6['powerflow_api'])
 
-        return {'sensor_api': sensor_api}
 
-result10 = sitesensors.sensordata()
-print(result10['sensor_api'])
+
+    class sitestorage():
+        @staticmethod
+        def storagedata():
+            storage_api = site_storage_json_data['storageData']
+
+            return {'storage_api': storage_api}
+
+
+    result7 = sitestorage.storagedata()
+    print(result7['storage_api'])
+
+
+
+    class siteenvbenefits():
+        @staticmethod
+        def envdata():
+            envbenefits_api = site_env_benefits_json_data['envBenefits']
+
+            return {'envbenefits_api': envbenefits_api}
+
+
+    result8 = siteenvbenefits.envdata()
+    print(result8['envbenefits_api'])
+
+
+
+    class siteinverter():
+        @staticmethod
+        def inverterdata():
+            inverter_api = site_inverter_json_data['data']
+
+            return {'inverter_api': inverter_api}
+
+    result9 = siteinverter.inverterdata()
+    #print(result9['inverter_api'])
+
+
+    class sitesensors():
+        @staticmethod
+        def sensordata():
+            sensor_api = site_sensor_json_data
+
+            return {'sensor_api': sensor_api}
+
+    result10 = sitesensors.sensordata()
+    print(result10['sensor_api'])
+
+
+
+
+
 
 
